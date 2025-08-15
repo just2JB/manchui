26e4bc46b7cdda3b37c59ecb31f9290b6228c1d6
just2JB <choijibeom@gamil.com>
2025-08-15 15:51:43 +0000

    시작

--- manchui_web/client/src/components/Footer/Footer.jsx
+++ manchui_web/client/src/components/Footer/Footer.jsx
@@ -7,12 +7,15 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="link-div">
-        <a href="https://www.instagram.com/maaaaaaanchui/" target="_blank">
-          <IoLogoInstagram size={20} />
-        </a>
-        <a href="https://www.youtube.com/@manchui10007" target="-blank">
-          <IoLogoYoutube size={20} />
-        </a>
+        <div>
+          <a href="https://www.instagram.com/maaaaaaanchui/" target="_blank">
+            <IoLogoInstagram size={20} />
+          </a>
+          <a href="https://www.youtube.com/@manchui10007" target="-blank">
+            <IoLogoYoutube size={20} />
+          </a>
+        </div>
+
         <Link>가입 하기</Link>
       </div>
 
@@ -21,8 +24,8 @@ const Footer = () => {
           Dance Crew. <span className="span-red">MANCHUI</span>
         </p>
         <p>
-          경기도 안산시 상록구 한양대학로 55, 한양대학교 ERICA캠퍼스 학생복지관
-          ~~~호
+          경기도 안산시 상록구 한양대학로 55<br></br> 한양대학교 ERICA캠퍼스
+          학생복지관 ~~~호
         </p>
         <p>jb040222@hanyang.ac.kr</p>
       </div>

daf0a2757c79b57858e1414515f9bc953dc7797e
그냥제비 <choijibeom@gmail.com>
2025-10-03 16:23:54 +0000

    설정창 끄적끄적

--- manchui_web/client/src/pages/Admin/AdminSetting.jsx
+++ manchui_web/client/src/pages/Admin/AdminSetting.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import "./AdminSetting.css";
+const clientURL = import.meta.env.VITE_CLIENT_URL;
 
 const AdminSetting = () => {
   return (
     <div>
       <div className="joinSetting">
+        <iframe
+          className="joinFrame"
+          title="joinForm"
+          src={`${clientURL}/join`}
+          width="400"
+          height="300"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          로드 중…
+        </iframe>
         <button className="openJoin">웹 가입신청 폼 활성화</button>
         <button className="closeJoin">웹 가입신청 폼 비활성회</button>
       </div>

590b2a1ba4ffc79c5ea12e530f7dbc8c989c538d
그냥제비 <choijibeom@gmail.com>
2025-11-13 15:47:49 +0000

    바텀 바 상태 바꾸기 테스트

--- manchui_web/client/src/pages/ClubRoom/BottomBar.jsx
+++ manchui_web/client/src/pages/ClubRoom/BottomBar.jsx
@@ -7,27 +7,54 @@ import {
   LiaCalendarCheckSolid,
 } from "react-icons/lia";
 import { RiMenuAddLine } from "react-icons/ri";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./BottomBar.css";
 
 const BottomBar = () => {
+  const location = useLocation();
+
   return (
     <div className="bottomBar">
-      <Link className="menu-button" to="/club">
+      <Link
+        className={`menu-button ${
+          location.pathname === "/club" ? "selcetMenu" : ""
+        }`}
+        to="/club"
+      >
         <LiaHomeSolid className="button-icon" />홈
       </Link>
-      <Link className="menu-button" to="/club/reservation">
+      <Link
+        className={`menu-button ${
+          location.pathname === "/club/reservation" ? "selcetMenu" : ""
+        }`}
+        to="/club/reservation"
+      >
         <LiaCalendarCheckSolid className="button-icon" />
         동아리방 예약
       </Link>
-      <Link className="menu-button" to="/club/practice">
+      <Link
+        className={`menu-button ${
+          location.pathname === "/club/practice" ? "selcetMenu" : ""
+        }`}
+        to="/club/practice"
+      >
         <LiaFlag className="button-icon" />
         연습
       </Link>
-      <Link className="menu-button" to="/club">
+      <Link
+        className={`menu-button ${
+          location.pathname === "/club/ww" ? "selcetMenu" : ""
+        }`}
+        to="/club"
+      >
         <RiMenuAddLine className="button-icon" />빈 블럭
       </Link>
-      <Link className="menu-button" to="/club/mypage">
+      <Link
+        className={`menu-button ${
+          location.pathname === "/club/mypage" ? "selcetMenu" : ""
+        }`}
+        to="/club/mypage"
+      >
         <LiaAddressCard className="button-icon" />
         마이 페이지
       </Link>

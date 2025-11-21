89cf25f92a5e237031cff85b5193114f27775c73
그냥제비 <choijibeom@gmail.com>
2025-11-21 19:53:32 +0000

    연습 페이지 디자인 및 리스트 수정

--- manchui_web/client/src/pages/ClubRoom/Practice/Practice.jsx
+++ manchui_web/client/src/pages/ClubRoom/Practice/Practice.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import "./Practice.css";
-import { IoAdd } from "react-icons/io5";
-import { BsBoxArrowInRight } from "react-icons/bs";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 import { useEffect } from "react";
+import {
+  MdOutlineOutlinedFlag,
+  MdOutlineAccessTime,
+  MdOutlinePlace,
+} from "react-icons/md";
 import axios from "axios";
 import Loading from "../../../components/Loading/Loading";
 import { useState } from "react";
@@ -13,16 +18,15 @@ const serverUrl = import.meta.env.VITE_SERVER_URL;
 //팀 id로 메인 이동
 
 const Practice = () => {
-  const [myTeams, setMyTeams] = useState([]);
+  const [newOpen, setNewOpen] = useState(false);
   const [practices, setPractices] = useState([]);
   const [seePractices, setSeePractices] = useState([]);
   const [selcetDay, setSelcetDay] = useState(new Date());
   const { user } = useOutletContext();
   const nav = useNavigate();
-  const nextDate = (num) => {
-    const nextDate = new Date();
-    nextDate.setDate(selcetDay.getDate() + num);
-    setSelcetDay(nextDate);
+
+  const openNewPractice = () => {
+    setNewOpen(true);
   };
 
   const changeUser = (userId) => {
@@ -36,71 +40,99 @@ const Practice = () => {
   };
 
   useEffect(() => {
-    const getMyTeams = async () => {
-      try {
-        const response = await axios.get(
-          `${serverUrl}/api/team/user/${user._id}`,
-          {
-            withCredentials: true,
-          }
-        );
-        setMyTeams(response.data.myTeam);
-      } catch {
-        alert(error.response.data.message);
-      }
-    };
     const getPractices = async () => {
       try {
         const response = await axios.get(`${serverUrl}/api/practice`, {
           withCredentials: true,
         });
-        const sortedPractices = response.data.practices.sort((a, b) => {
-          const aFirst = a.time.split("~")[0];
-          const bFirst = b.time.split("~")[0];
-          if (aFirst > bFirst) return 1;
-          if (aFirst === bFirst) return 0;
-          if (aFirst < bFirst) return -1;
+        const dateSorted = response.data.practices.sort((a, b) => {
+          if (a.date > b.date) return 1;
+          if (a.date === b.date) {
+            const aFirst = a.time.split("~")[0];
+            const bFirst = b.time.split("~")[0];
+            if (aFirst > bFirst) return 1;
+            if (aFirst === bFirst) return 0;
+            if (aFirst < bFirst) return -1;
+          }
+          if (a.date < b.date) return -1;
         });
-        setPractices(sortedPractices);
-        setSeePractices(sortedPractices);
+        setPractices(dateSorted);
+        setSeePractices(dateSorted);
       } catch {
         alert(error.response.data.message);
       }
     };
     getPractices();
-    getMyTeams();
   }, []);
   return (
     <div className="practice">
-      <div className="weekSelector">엄;</div>
-      <div className="myPractice">
-        <div className="topMenu"></div>
-        <div className="practicesBottom">
-          <div className="listControler">
-            <div onClick={() => nextDate(-1)}>전날</div>
-            <div onClick={() => changeUser(user._id)} className="">
-              내 연습만 보기
-            </div>
-            <div onClick={() => changeUser("All")} className="">
-              모두 보기
-            </div>
-            <div onClick={() => nextDate(1)}>다음날</div>
+      <div className="topMenu">
+        <div className="weekInfo">11월</div>
+        <div className="weekSelector">
+          <Swiper
+            className="weekSwiper"
+            loop="true"
+            slidesPerView={1}
+            spaceBetween={10}
+            resistanceRatio={0}
+            speed={400}
+          >
+            <SwiperSlide className="weekSlide">
+              <div className="weeks">
+                {[1, 2, 3, 4, 5, 6, 7].map((item) => (
+                  <div key={item} className="weekDay mok">
+                    <div className="dateText">{item}</div>
+                    <div className="dayText">요일</div>
+                    <div className="datePractices">
+                      {["연습1", "연습2", "연습3"].map((practice) => (
+                        <div
+                          key={practice}
+                          className="datePracticeObject"
+                        ></div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="weekSlide"></SwiperSlide>
+            <SwiperSlide className="weekSlide"></SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="listControler">
+          <div className="controlButton" onClick={() => changeUser(user._id)}>
+            내 연습만 보기
           </div>
+          <div className="controlButton" onClick={() => changeUser("All")}>
+            모두 보기
+          </div>
+        </div>
+      </div>
+
+      <div className="myPractice">
+        <div className="practicesBottom">
           <div className="practiceList">
-            <div>{selcetDay.toLocaleDateString()}</div>
-            <div className="listPracticeCard listIndex">
-              <div>팀</div>
-              <div>시간</div>
-              <div>연습실</div>
-            </div>
-            {seePractices
-              .filter(
-                (practice) =>
+            {seePractices.map((practice, index) => (
+              <div
+                key={practice._id}
+                className={`practiceCard ${
                   new Date(practice.date).toLocaleDateString() ===
                   selcetDay.toLocaleDateString()
-              )
-              .map((practice) => (
-                <div key={practice._id} className="listPracticeCard">
+                    ? "selectDateCard"
+                    : ""
+                }`}
+                onClick={() => setSelcetDay(new Date(practice.date))}
+              >
+                {index === 0 ||
+                practice.date !== seePractices[index - 1].date ? (
+                  <div className={`practiceDateText`}>
+                    {practice.date.slice(0, 4)}년 {practice.date.slice(5, 8)}월{" "}
+                    {practice.date.slice(9, 12)}일
+                  </div>
+                ) : (
+                  ""
+                )}
+                <div className="listPracticeCard">
                   <div>{practice.teamName}</div>
                   <div>
                     {(practice.time.split("~")[0] * 2) % 2 === 0
@@ -112,13 +144,34 @@ const Practice = () => {
                       : `${practice.time.split("~")[1] - 0.5}:30`}
                   </div>
 
-                  <div>{practice.place}</div>
+                  <div>
+                    <MdOutlinePlace />
+                    {practice.place}
+                  </div>
                 </div>
-              ))}
+              </div>
+            ))}
           </div>
         </div>
       </div>
-      <div className="floatAddPractice">+새 연습</div>
+      <div className="floatAddPractice" onClick={() => openNewPractice()}>
+        +새 연습
+      </div>
+      {newOpen ? (
+        <div className="newPractice">
+          <div className="closeSection" onClick={() => setNewOpen(false)}></div>
+          <div className="buttonBox">
+            <button onClick={() => nav("/club/team/create-team")}>
+              새 팀에서 만들기
+            </button>
+            <button onClick={() => nav("/club/team")}>
+              기존 팀 연습 추가하기
+            </button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
@@ -132,5 +185,5 @@ export default Practice;
 4. 연습 추가 누르면  / [새로운 팀 만들기 => 팀 만들기 => 링크 공유 유도] / [기존팀에서 추가 => 팀 페이지로 이동]
 5. 월간 선택, 보기 만들어서 한눈에 보기
 6. 스케줄 작성 페이지 만들고 홈, 스케줄 페이지에 에 팀 스케줄 요청된 날짜 강조해주는 효과 만들기
-
+7. 연습데이터가 나중엔 개 많을거니까 잘라서 가져오게 바꿔야할듯 (ALL) (date~date)
 */
